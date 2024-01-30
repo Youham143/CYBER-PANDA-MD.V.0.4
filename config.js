a0d76f9142cd23d80c6d9e7bf4a6ded11958d02b
Youham143 <158036125+Youham143@users.noreply.github.com>
2024-01-30 13:50:15 +0500

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 //
 global.Owner = ["+923178511210", "+923178511210"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+923178511210", "+923178511210"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
 global.ownertag = ["+923178511210"];
 global.OwnerName = "HAMZA";
 global.BotName = "DARK KHAN";
